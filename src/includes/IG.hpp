49b3f6ee4439367f280da70b73fc971e78d00916
skyleaworlder <870033938@qq.com>
2020-11-10 00:04:03 +0800

    D: reset relationship still...

--- src/includes/IG.hpp
+++
@@ -1,8 +0,0 @@
-#include <cmath>
-#include <limits>
-#include <iostream>
-
-double gammln(const double input);
-std::pair<double, double> gser(const double alpha, const double x);
-double gammp(const double a, const double x);
-double gammq(const double a, const double x);
