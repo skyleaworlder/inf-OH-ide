e2eb56ea87517286bd5d5404e9690ce3269a33e3
Eol <hslphst@gmail.com>
2020-11-16 00:52:49 +0800

    BMP infoHeader Reading Finished

--- ASS-watermarking/bmp.hpp
+++ ASS-watermarking/bmp.hpp
@@ -1,39 +1,39 @@
 #include <cstddef>
-#include <iostream>
 #include <string>
 
 using Byte = unsigned char;
 using ByteString = std::basic_string<Byte>;
 
-struct BitmapHeader {
-    Byte signature[2];  // = "BM"
-    uint32_t fileSize;
-    Byte reserved[4];
-    uint32_t dataOffset;
+struct __attribute__ ((__packed__)) BitmapHeader {
+    Byte        signature[2];  // = "BM"
+    uint32_t    fileSize;
+    Byte        reserved[4];
+    uint32_t    dataOffset;
 };
 
-struct BitmapInfoHeader {
-    uint32_t size;  // = 40
-    uint32_t width, height;
-    uint16_t planes;    // = 1
-    uint16_t bitsPerPixel;
-    uint16_t compression;
-    uint16_t imageSize;
-    uint16_t xPixelsPerM, yPixelsPerM;
-    uint16_t colorsUsed;
-    uint16_t importantColors;
+struct __attribute__ ((__packed__)) BitmapInfoHeader {
+    uint32_t    size;  // = 40
+    uint32_t    width, height;
+    uint16_t    planes;    // = 1
+    uint16_t    bitsPerPixel;
+    uint32_t    compression;
+    uint32_t    imageSize;
+    uint32_t    xPixelsPerM, yPixelsPerM;
+    uint32_t    colorsUsed;
+    uint32_t    importantColors;
 };
 
-struct ColorTable {
-    Byte red, green, blue, reserved;
-};
+// struct ColorTable {
+//     Byte red, green, blue, reserved;
+// };
 
-class BMP {
-    BitmapHeader header;
-    BitmapInfoHeader infoHeader;
-    ColorTable colorTable;
-    ByteString pixelData;
+class __attribute__ ((__packed__)) BMP {
+    BitmapHeader        _header;
+    BitmapInfoHeader    _infoHeader;
+    // ColorTable          _colorTable;
+    ByteString          _pixelData;
+    unsigned int _numColors;
 public:
-    BMP() = default;
+    BMP(std::string bmpFilePath);
     ~BMP() = default;
 };
