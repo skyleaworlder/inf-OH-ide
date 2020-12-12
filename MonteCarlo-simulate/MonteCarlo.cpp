b93b8525d2f3e168600f335b74f68102688ccdb8
skyleaworlder <870033938@qq.com>
2020-12-12 19:50:41 +0800

    M: fix ierfc && add addVoice func

--- MonteCarlo-simulate/MonteCarlo.cpp
+++ MonteCarlo-simulate/MonteCarlo.cpp
@@ -12,6 +12,16 @@ void MC::MonteCarlo::imbed(const double a) {
     }
 }
 
+void MC::MonteCarlo::addVoice(const double STD) {
+    std::vector<double> voice_H0 { GD_vector(0.0, STD, this->N) };
+    std::vector<double> voice_H1 { GD_vector(0.0, STD, this->N) };
+
+    for (size_t index = 0; index < this->N; index++) {
+        this->S_H0[index] += voice_H0[index];
+        this->S_H1[index] += voice_H1[index];
+    }
+}
+
 void MC::MonteCarlo::genLinearCorrelator() {
     double sum_Si_wi_H0 = 0;
     double sum_Si_wi_H1 = 0;
@@ -25,12 +35,14 @@ void MC::MonteCarlo::genLinearCorrelator() {
     this->L_S_H1 = sum_Si_wi_H1 / this->N;
 }
 
-constexpr bool MC::MonteCarlo::isMiss(const double psi) const {
-    return (this->L_S_H1 < psi);
+double MC::MonteCarlo::calcuPm(const double psi, const double a) {
+    double x = sqrt(this->N)*(psi - a) / sqrt(MC::variance(this->X));
+    return 1 - MC::Q(x);
 }
 
-constexpr bool MC::MonteCarlo::isFalseAlarm(const double psi) const {
-    return (this->L_S_H0 > psi);
+double MC::MonteCarlo::calcuPfa(const double psi, const double a) {
+    double x = sqrt(this->N)*psi / sqrt(MC::variance(this->X));
+    return MC::Q(x);
 }
 
 double MC::mean(const std::vector<double> X) {
@@ -44,9 +56,11 @@ double MC::mean(const std::vector<double> X) {
 double MC::variance(const std::vector<double> X) {
     double x_square = 0;
     double x_mean = MC::mean(X);
+
     std::for_each(X.begin(), X.end(), [&x_square, x_mean](double x) {
         x_square += pow(x-x_mean, 2);
     });
+
     return x_square / X.size();
 }
 
@@ -57,4 +71,12 @@ double MC::calcuPmTheory(
     const double sigma_X
 ) {
     return 1-MC::Q(MC::iQ(pfa) - sqrt(N)*a/sigma_X);
+}
+
+double MC::calcuPsiTheory(
+    const double sigma_X,
+    const double pfa,
+    const size_t N
+) {
+    return sigma_X * MC::iQ(pfa) / sqrt(N);
 }
--- MonteCarlo-simulate/MonteCarlo.hpp
+++ MonteCarlo-simulate/MonteCarlo.hpp
@@ -9,6 +9,8 @@
 #include <vector>
 #include <algorithm> // for std::for_each
 #include <array>
+#include <cassert>
+#include <iostream>
 
 MC_NP_BEG
 
@@ -31,47 +33,63 @@ public:
     ~MonteCarlo() {};
 
     /**
-     * @brief 嵌入水印的函数
+     * @brief 嵌入水印的函数，
+     *          通过这个函数修改 a
+     *          也是通过这个函数初始化 S_H0, S_H1
      * @param a: 嵌入强度
      */
     void imbed(const double a);
 
     /**
+     * @brief 对 S_H0 和 S_H1 施加噪声
+     *          需要注意的是，这是对 S_H0 / S_H1 的直接修改
+     *          接下来如果继续添加噪声，噪声会叠加
+     * @param STD: double，标准差
+     */
+    void addVoice(const double STD);
+
+    /**
      * @brief 生成线性相关器 L(S), L(S|H1), L(S|H0)
+     *          由于是根据 S_H0, S_H1 来计算的
+     *          与 S_H0, S_H1 相关
+     *          因此不需要给嵌入强度 a
+     *
+     *          会产生 L_S_H0 和 L_S_H1
      */
     void genLinearCorrelator();
 
     /**
      * @brief 计算是否漏检
+     *          根据 L_S_H1 计算
      *          pm = P(detector says H0 | H1)
      *             = P(1/N \sum_{i=1}^N X_i w_i + a < \psi)
      * @param ψ: double，决策阈值
      */
-    constexpr bool isMiss(const double psi) const;
+    constexpr bool isMiss(const double psi) const {
+        return (this->L_S_H1 < psi);
+    }
 
     /**
      * @brief 计算是否误检
+     *          根据 L_S_H0 计算
      *          pfa = P(detector says H1 | H0)
      *              = P(1/N \sum_{i=1}^N X_i w_i >= \psi)
      * @param ψ: double，决策阈值
      */
-    constexpr bool isFalseAlarm(const double psi) const;
+    constexpr bool isFalseAlarm(const double psi) const {
+        return (this->L_S_H0 > psi);
+    }
 
     /**
      * @brief 计算漏检概率 slides P28
+     *          根据 X 计算
      */
-    inline double calcuPm(const double psi, const double a) {
-        double x = sqrt(this->N)*(psi - a) / MC::variance(this->X);
-        return 1 - Q(x);
-    }
+    double calcuPm(const double psi, const double a);
 
     /**
      * @brief 计算误检概率 slides P30
      */
-    inline double calcuPfa(const double psi, const double a) {
-        double x = sqrt(this->N)*psi / MC::variance(this->X);
-        return Q(x);
-    }
+    double calcuPfa(const double psi, const double a);
 };
 
 /**
@@ -107,12 +125,27 @@ double calcuPmTheory(
 );
 
 /**
+ * @brief 计算理论阈值 slides-实验二说明 P12
+ * @param sigma_X: double，给出标准差
+ * @param pfa: double，给出用于计算理论阈值的理论 pfa
+ * @param N: size_t，样本总数
+ */
+double calcuPsiTheory(
+    const double sigma_X,
+    const double pfa,
+    const size_t N
+);
+
+/**
  * @brief Q(x) = \int_x^{+\inf} \frac{1}{\sqrt{2\pi}} exp(-t^2 / 2) dt
+ *          Q(x) = erfc(x) / 2
  * @param x: double，输入
  * @return Q(x)
  */
 inline double Q(const double x) {
-    return erfc(x) / 2;
+    //std::cout << x << '\n';
+    // x \in (-\inf, +\inf), y \in (0, 1)
+    return erfc(x) / 2.0;
 }
 
 /**
@@ -121,12 +154,15 @@ inline double Q(const double x) {
  * @return iQ(x)
  */
 inline double iQ(const double x) {
+    //std::cout << x << '\n';
+    // x \in (0, 1), y \in (-\inf, +\inf)
+    assert(x > -0.01 && x < 1.01);
 /**
  *    Q(x) = erfc(x) / 2, efrc(iefrc(x)) = x
- * => 2Q(iefrc(x)) = x
- * => Q^{-1}(x) = 2*iefrc(x)
+ * => 2Q(iefrc(x)) = x => Q(iefrc(x)) = x/2 => iefrc(x) = Q^{-1}(x/2)
+ * => Q^{-1}(x) = iefrc(2*x)
  */
-    return ierfc(x) * 2;
+    return ierfc(x*2.0);
 }
 
 MC_NP_END
